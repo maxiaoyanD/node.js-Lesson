f58bf127ac8e2e104fb8bbdb2f46722308adcdc9
mxyD <2390140586@qq.com>
2019-09-23 14:04:57 +0800

    更新

--- （3）NodeJS事件/listenRadio.js
+++ （3）NodeJS事件/listenRadio.js
@@ -1,11 +1,15 @@
+const events = require("events");
+const util = require('util');
+const EventEmitter = events.EventEmitter;
 var listenRadio = require("./radio.js");
 
-var radio = new listenRadio("music radio" ,"FM 106.7 ");
-
-    radio.on("opened",radio.play);
-    console.log("music radio FM 106.7 opened");
-    var intervalId = setTimeout(function(){
-        radio.emit("opened");
-    },2000);
-    radio.on("closed",radio.stop);
+var radio = new listenRadio.Radio("music radio" ,"FM 106.7 ");
+util.inherits(listenRadio.Radio, EventEmitter);    
+radio.on("opened",radio.play);
+radio.on("closed",radio.stop);
+radio.emit("opened");
+setTimeout(function(){
     radio.emit("closed"); 
+},2000);
+    
+    
--- （3）NodeJS事件/radio.js
+++ （3）NodeJS事件/radio.js
@@ -1,16 +1,19 @@
 
-const util = require("util")
-const events = require("events")
+const events = require("events");
 var eventsEmitter = events.EventEmitter;
 function Radio(radioName,radioFm){
+    eventsEmitter.call(this);
     this.radioName = radioName;
     this.radioFm = radioFm;
 }
 Radio.prototype.play = function(){
+        console.log("music radio FM 106.7 opended");
         console.log("lalalala");
 };
 Radio.prototype.stop = function(){
     console.log(this.radioName + " " + this.radioFm + " closed");
-    clearInterval(intervalId);
+
 };
-util.inherits(Radio,eventsEmitter);
+module.exports={
+    Radio:Radio
+}
